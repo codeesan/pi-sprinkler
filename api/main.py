a0e3fa19f79f56549cff70943a70d3685bd94015
Cody Chandler <cody.spg@gmail.com>
2024-04-25 13:23:46 -0700

    update to sqlite_put with RETURNING

--- api/main.py
+++ api/main.py
@@ -44,7 +44,7 @@ def update_valve(valve: Valve, valve_id: int):
     name = update_valve_encoded["name"]
     description = update_valve_encoded["description"]
     
-    result = sqlite_put("valves",valve_id,"update valves set name=\"{0}\", description=\"{1}\" where id={2}".format(name,description,valve_id))
+    result = sqlite_put("valves","update valves set name=\"{0}\", description=\"{1}\" where id={2}".format(name,description,valve_id))
 
     return {"data":result}
 
@@ -63,5 +63,5 @@ def update_zone_name(zone_id: int, zone: ZoneName):
     update_zone_name_encoded = jsonable_encoder(zone)
     name = update_zone_name_encoded["name"]
     description = update_zone_name_encoded["description"]
-    result = sqlite_put("zone_names", zone_id, "update zone_names set name=\"{0}\", description=\"{1}\" where id={2}".format(name,description,zone_id))
+    result = sqlite_put("zone_names", "update zone_names set name=\"{0}\", description=\"{1}\" where id={2}".format(name,description,zone_id))
     return{"data":result}
--- api/sprinkerfunctions.py
+++ api/sprinkerfunctions.py
@@ -12,18 +12,27 @@ def sqlite_to_dict(statement):
     con.close()
     return result
 
-def sqlite_put(table,id,statement):
+def sqlite_put(table,statement):
     result = []
     con = sqlite3.connect("sprinklers.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute(statement)
-    con.commit()
-    cur = con.cursor()
-    select_statement = "select * from {0} where id={1}".format(table,id)
-    for row in cur.execute(select_statement):
-        print(dict(row))
-        result.append(dict(row))
+    cur.execute(statement + " RETURNING *; ")
+    result = cur.fetchall()
     con.commit()
     con.close()
     return result
+
+# def sqlite_post(table,statement):
+#     con = sqlite3.connect("sprinklers.db")
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.execute(statement)
+#     con.commit()
+#     cur = con.cursor()
+#     select_statement = "select * from {0} where id={1}".format(table,id)
+#     for row in cur.execute(select_statement):
+#         print(dict(row))
+#         result.append(dict(row))
+#     con.commit()
+#     con.close()
