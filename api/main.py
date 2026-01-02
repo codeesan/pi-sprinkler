0f90c669cb8cf30d46872ea28e8f94ad1156d69b
Cody Chandler <cody.spg@gmail.com>
2026-01-02 13:34:37 -0800

    test

--- api/main.py
+++ api/main.py
@@ -104,7 +104,7 @@ def operate_valve(valve:int, set_status:str, response:Response):
         return{"data":"Valve {0} set to {1}. result: {2}".format(valve,set_status,result)}
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
-        return{"data":"Aborting - Something went wrong. result: {0}".format(result)}
+        return{"data":"1 Aborting - Something went wrong. result: {0}".format(result)}
     
 @app.get("/valves/checkstatus", status_code=200)
 def check_valve_status(valve:int, response:Response):
